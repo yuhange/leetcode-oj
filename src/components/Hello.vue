62314c4954e90731786f5ca38a542bd43330be58
yuhange <w868433@163.com>
2016-12-26 16:05:29 +0800

    first commit

---
+++ src/components/Hello.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="hello">
+    <h1>leetcode</h1>
+    <ul v-for="question in questions">
+      <li>{{question.ques_title}}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+      questions: [],
+      url: 'http://localhost:3000/leetcode'
+    }
+  },
+  created() {
+    this.fetchData()
+  },
+  methods: {
+    fetchData: function() {
+      this.$http.get(this.url).then(response=>{
+        this.questions = response.body.data
+        console.log(response.body.data[0])
+      })
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
